4edec2b91c3b0d8e0e7566c6a78f34c3229b2647
Facundo Bieri <fbieri@mail.austral.edu.ar>
2025-08-05 20:32:01 -0300

    DbContext

---
+++ Infrastructure/OrderDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class OrderDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public OrderDBContext() { }
+        public OrderDBContext(DbContextOptions<OrderDBContext> options) : base(options) { }
+    }
+}
