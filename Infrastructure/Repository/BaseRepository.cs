ab915b7a9c22986671fffb96ea9a67ee20998ce6
Franco aranibar <acuquita2003@hotmail.com>
2025-08-09 01:34:44 -0300

    Generic User in process

---
+++ Infrastructure/Repository/BaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        private readonly OrderDBContext _dbContext;
+
+        public BaseRepository(OrderDBContext dBContext) 
+        {
+            _dbContext = dBContext;
+        }
+
+        public List<T> Get()
+        {
+            return _dbContext.Set<T>().ToList();
+        }
+
+        public T Get<TId>(TId id)
+        {
+            return _dbContext.Set<T>().Find(new object[] { id });
+        }
+
+    }
+}
