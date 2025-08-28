0a375b6bca4643bc9a983a8dcf035abe30cd27c5
rodrimadrid21 <madrid.rodrigo21@gmail.com>
2025-08-28 16:24:32 -0300

    Entidades branch y menu

--- Domain/Entities/Branch.cs
+++ Domain/Entities/Branch.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace Domain.Entities
 {
     public class Branch
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        // Relaciones 1 - N
+        public ICollection<Table> Tables { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public ICollection<User> Users { get; set; }
+        public ICollection<Menu> Menus { get; set; }
     }
 }
--- Domain/Entities/Menu.cs
+++ Domain/Entities/Menu.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
     public class Menu
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        // Relación con sucursal
+        public int BranchId { get; set; }
+        public Branch Branch { get; set; }
+
+        // Productos que contiene el menú
+        public ICollection<Product> Products { get; set; }
     }
 }
